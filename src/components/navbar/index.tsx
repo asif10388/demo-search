7e832cddcf689d786a9c2689faf81f8bd20181cf
asif10388 <asif.khan10388@gmail.com>
2021-11-21 22:19:45 +0600

    fixed errors

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <header className="bg-white text-gray-600 body-font">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+        <a href="www.sharetrip.net" className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
           <img src="https://res.cloudinary.com/asif10388/image/upload/v1637438288/sharetrip/Full_iwfxm9.png" alt="logo" className="w-32 object-contain object-center" />
         </a>
         <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
